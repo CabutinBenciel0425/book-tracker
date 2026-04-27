6a931d6c4043f72f8df6f3217f8fc0af145b2d7a
Benciel Cabutin <cabutinb@gmail.com>
2026-04-27 14:03:23 +0800

    Starter Files

---
+++ README.md
@@ -0,0 +1,5 @@
+## Book Tracker application
+
+A book tracker application built with React, TypeScript, Tailwind CSS, and
+React Router, featuring centralized state management using the Context API
+and useReducer.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      A book tracker application built with React, TypeScript, Tailwind CSS, and
+      React Router, featuring centralized state management using the Context API
+      and useReducer.
+    </div>
+  );
+}
+
+export default App;
